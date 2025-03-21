e93a73a03ecc0a64635ca6a07612eac5435fa72d
Hank Barta <hbarta@gmail.com>
2025-03-21 09:06:36 -0500

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,22 @@
 
 AsyncUDP udp;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin();
-  while(WiFi.waitForConnectResult() != WL_CONNECTED) {
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.println("no wifi yet");
     delay(1000);
   }
   Serial.println("yes wifi yet");
-  if (udp.connect(IPAddress(255, 255, 255, 255), 5016)) {
+  if (udp.connect(IPAddress(255, 255, 255, 255), 5016))
+  {
     Serial.println("UDP connected");
-    udp.onPacket([](AsyncUDPPacket packet) {
+    udp.onPacket([](AsyncUDPPacket packet)
+                 {
       Serial.print("UDP Packet Type: ");
       Serial.print(packet.isBroadcast() ? "Broadcast" : packet.isMulticast() ? "Multicast" : "Unicast");
       Serial.print(", From: ");
@@ -32,14 +36,14 @@ void setup() {
       Serial.write(packet.data(), packet.length());
       Serial.println();
       //reply to the client
-      packet.printf("Got %u bytes of data", packet.length());
-    });
-    //Send unicast
+      packet.printf("Got %u bytes of data", packet.length()); });
+    // Send unicast
     udp.print("Hello Server!");
   }
 }
 
-void loop() {
+void loop()
+{
   Serial.println("looping");
   udp.broadcastTo("Anyone here?", 5016);
   delay(1000);
