5d9254e8d57ba894470247fd7226b5561e656f39
Hank Barta <hbarta@gmail.com>
2025-03-21 09:21:57 -0500

    send correct config requiest

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <AsyncUDP.h>
+#include <esp_wifi.h>
 
 AsyncUDP udp;
+
+static const size_t bcast_buff_len = 30;
+static char bcast_buff[bcast_buff_len];
 
 void setup()
 {
@@ -38,13 +42,30 @@ void setup()
       //reply to the client
       packet.printf("Got %u bytes of data", packet.length()); });
     // Send unicast
-    udp.print("Hello Server!");
+    uint8_t baseMac[6];
+    esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+    if (ret == ESP_OK)
+    {
+      Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                    baseMac[0], baseMac[1], baseMac[2],
+                    baseMac[3], baseMac[4], baseMac[5]);
+      snprintf(bcast_buff, bcast_buff_len,
+               "request-config|%02x%02x%02x%02x%02x%02x",
+               baseMac[0], baseMac[1], baseMac[2],
+               baseMac[3], baseMac[4], baseMac[5]);
+      udp.print(bcast_buff);
+    }
+    else
+    {
+      Serial.println("Failed to read MAC address");
+    }
   }
 }
 
 void loop()
 {
   Serial.println("looping");
-  udp.broadcastTo("Anyone here?", 5016);
+  udp.broadcastTo(bcast_buff, 5016);
+  // udp.print(bcast_buff); also works.
   delay(1000);
 }
